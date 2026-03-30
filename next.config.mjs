1638adf1f29001d33b16a15baed12cd4ce21b2e9
ftcaleb <caleb19scott@gmail.com>
2026-03-30 12:49:50 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
+  async rewrites() {
+    return [
+      {
+        source: '/moodle/:path*',
+        destination: 'http://143.110.154.83/:path*',
+      },
+    ];
   },
-  images: {
-    unoptimized: true,
-  },
-}
+};
 
-export default nextConfig
+export default nextConfig;
